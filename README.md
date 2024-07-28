2970a11b58058633fe23860d892062d64b97bf7e
Tommaso Brega <tommy.brega@gmail.com>
2024-07-28 17:41:16 -0300

    Restaurant Info Update
    - Takes into consideration the restaurant information that have been fed trough a json

--- README.md
+++ README.md
@@ -35,6 +35,7 @@ Test with Ngrok
 
 Userfull links:
     [Inspiration](https://www.twilio.com/en-us/blog/ai-chatbot-whatsapp-python-twilio-openai)
+    [Userfull API Info](https://www.twilio.com/docs/messaging/api/message-resource)
     [Troubleshhoting](https://stackoverflow.com/questions/69934467/error-while-using-createdb-command-postgresql-on-windows-10)
     [Troubleshhoting](https://stackoverflow.com/questions/48999379/psycopg2-operationalerror-fatal-password-authentication-failed-for-user-my-u)
     [Troubleshooting](https://stackoverflow.com/questions/60138692/sqlalchemy-psycopg2-errors-insufficientprivilege-permission-denied-for-relation)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Third-party imports
+import json
 import openai
 from fastapi import FastAPI, Form, Depends
 from decouple import config
@@ -23,12 +24,24 @@ def get_db():
     finally:
         db.close()
 
-def get_information_from_chatgpt(web_content):
+# Function to read JSON file
+def read_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def get_information_from_chatgpt(question_string, json_reference):
+
+    # Convert the JSON reference to a string
+    json_reference_str = json.dumps(json_reference, indent=2)
+
+    question_str = "Answer any quations only with information inside of the following json never go off topic if there is no answer to the question pelitly decline can you also give the answer in bullet points please" + json_reference_str + "The following is the question made by the user" + question_string
+
     stream = openai.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": web_content}],
+        messages=[{"role": "user", "content": question_str}],
         stream=True,
     )
+
      # Initialize an empty string to accumulate the response
     response = ""
     
@@ -39,15 +52,18 @@ def get_information_from_chatgpt(web_content):
     # Return the accumulated response
     return response
 
-@app.post("/message")
-async def reply(Body: str = Form(), db: Session = Depends(get_db)):
+@app.post("/webook")
+async def reply(Body: str = Form(), From: str = Form(), db: Session = Depends(get_db)):
+        # Read the JSON reference from the file
+    json_reference = read_json_file('Components/test_menu.json')
+
     # The generated text
-    chat_response = get_information_from_chatgpt(Body)
+    chat_response = get_information_from_chatgpt(Body, json_reference)
 
     # Store the conversation in the database
     try:
         conversation = Conversation(
-            sender=whatsapp_number,
+            sender=From,
             message=Body,
             response=chat_response
             )
